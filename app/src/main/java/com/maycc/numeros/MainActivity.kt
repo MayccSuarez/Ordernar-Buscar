54603a56938f37cae1b59d1c095f898db2c029b0
= <=>
2018-08-12 17:19:08 -0500

    llenar list view

--- app/src/main/java/com/maycc/numeros/MainActivity.kt
+++ app/src/main/java/com/maycc/numeros/MainActivity.kt
@@ -3,16 +3,29 @@ package com.maycc.numeros
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var arrayListNumbers = arrayListOf<String>()
+    private var adapter :ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initAdapter()
+        initListViewNumbers()
         addListenerBtnEnter()
+    }
+
+    private fun initAdapter() {
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayListNumbers)
+    }
+
+    private fun initListViewNumbers() {
+        lvNumbers.adapter = adapter
     }
 
     private fun addListenerBtnEnter() {
@@ -20,10 +33,15 @@ class MainActivity : AppCompatActivity() {
             val number = edtNumber.text.toString()
 
             if (isEnterNumber(number)) {
-                Log.d("ENTER NUMBER", "Numero ingresado")
+                addNumberToArrayListNumbers(number)
             }
 
         }
     }
     private fun isEnterNumber (number :String) = number.isNotEmpty()
+
+    private fun addNumberToArrayListNumbers(number: String) {
+        arrayListNumbers.add(number)
+        adapter?.notifyDataSetChanged()
+    }
 }
